6a3dbfdd9b836f6009a607f75c17ded9cea59835
Rashitov R <r.rashitov@webbankir.ru>
2023-01-26 16:43:35 +0600

    show message dialog with selected folder

--- src/main/kotlin/com/example/gptplugin/HelloAction.kt
+++ src/main/kotlin/com/example/gptplugin/HelloAction.kt
@@ -2,10 +2,25 @@ package com.example.gptplugin
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.ui.Messages
 
 class HelloAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        Messages.showMessageDialog(e.project, "Hello world", "My Chat GPT", Messages.getInformationIcon())
+        val fileChooserDescriptor = FileChooserDescriptor(
+            false,
+            true,
+            false,
+            false,
+            false,
+            false
+        )
+        fileChooserDescriptor.title = "Select a directory"
+        fileChooserDescriptor.description = "Select a directory to be used as the root of the project"
+
+        FileChooser.chooseFile(fileChooserDescriptor, e.project, null) {
+            Messages.showMessageDialog(e.project, "You selected ${it.path}", "Information", Messages.getInformationIcon())
+        }
     }
 }
